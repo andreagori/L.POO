5b514ec2ce723a0584a34bc007ed2334bac4db65
andreagori <andrearivassg@gmail.com>
2024-02-13 22:50:49 -0800

    practicas

--- Ejemplos clase/persona.cpp
+++ Ejemplos clase/persona.cpp
@@ -1,38 +1,101 @@
 #include <iostream>
+#include <time.h>
+#include <string.h>
 #include "persona.h"
 
+#define _CRT_SECURE_NO_WARNINGS
 using std::cin;
 using std::cout;
 using std::endl;
 
 int main()
 {
+    srand(time(NULL)); // inicializa el generador de numeros aleatorios.
     // crear un objeto, existen dos formas de hacerlo.
-    Persona la_persona; // CREANDOLO DE FORMA ESTATICA.
+    Persona la_persona("Andrea", 19); // CREANDOLO DE FORMA ESTATICA.
+
+    // LA SEGUNDA FORMA ES DE MANERA DINAMICA, CON APUNTADOR
+    // Se debe crear por el apuntador new, devuelve la direccion de memoria del objeto creado en el heap.
+    Persona *nueva_persona;
+    nueva_persona = new Persona("Teresa", 19);
+    // La diferencia entre estos objetos, es que los estaticos se eliminan al final del programa,
+    // y los dinamicos se eliminan con delete. Se borran explicitamente.
+    // Se deben establecer la visibilidad de los atributos y metodos, si no se establece nada, por defecto es privado.
+
+    // La principal dierencia entre los objetos en memoria estatica y los objetos en memoria dinamica es que los objetos estaticos se eliminan
+    // automaticamente cuando el programa o bloque de codigo termina, mientras que los objetos dinamicos no. Es necesario liberar la memoria manualmente (delete)
+
+    // Para utilziar el objeto, podemos acceder a sus miembros (variables o funciones) por medio de los mismos operadores que utilizamos para las estructuras
+    //  . cuando es estatico
+    //  -> cuando es dinamico
+    la_persona.saludar();
+    la_persona.hacerreir(rand());
+    nueva_persona->saludar();
+    nueva_persona->hacerreir(rand());
+
+    Persona tercera_persona("Juan", 20);
+    tercera_persona.saludar();
+
+    delete nueva_persona; // se borra el objeto creado en el heap.
 }
-// void Persona::reir()
-// {
-//     cout << "JAJAJA" << endl;
-// }
+// Los constructores se utilizan para inicializar atributos de la clase. Y reservar memoria.
 
-// void Persona::comer()
-// {
-//     cout << "ÑAM ÑAM" << endl;
-// }
+Persona::Persona()
+{
+    // Los constructores se utilizan para inicializar atributos y reservr recursos que pudiera utilizar el objeto
+    edad = 20;
+    nombre = (char *)malloc(7);
+    strncpy(nombre, "Sosimo", 6); // Copia el contenido de la cadena de texto en el segundo argumento al primer argumento
+    nombre[6] = 0;
+}
 
-// void Persona::saludar()
-// {
-//     cout << "HOLA" << endl;
-// }
+Persona::Persona(const char *el_nombre, int la_edad) // constructor por defecto
+{
+    edad = la_edad;
+    nombre = (char *)malloc(strlen(el_nombre) + 1);
+    strncpy(nombre, el_nombre, strlen(el_nombre));
+    nombre[strlen(el_nombre)] = 0;
+}
 
-// void Persona::hacerreir(int motivo)
-// {
-//     if ((motivo % 3) == 0)
-//     {
-//         reir();
-//     }
-//     else
-//     {
-//         cout << ":/" << endl;
-//     }
-// }
+Persona::~Persona()
+{
+    if (nombre != nullptr)
+    {
+        cout << "Adios, " << nombre << endl;
+        free(nombre);
+    }
+}
+
+void Persona::reir()
+{
+    cout << "JAJAJA" << endl;
+}
+
+void Persona::comer()
+{
+    cout << "ÑAM ÑAM" << endl;
+}
+
+void Persona::saludar()
+{
+    cout << "Hola, soy " << nombre;
+    cout << " y tengo " << edad << endl;
+}
+
+void Persona::hacerreir(int motivo)
+{
+    if ((motivo % 3) == 0) // cuando tu codigo es de una linea no ocupas llaves, pero igual usarlas.
+    {
+        reir();
+    }
+    else
+    {
+        cout << ":/" << endl;
+    }
+}
+
+void Persona::liberar()
+{
+    free(nombre);
+    nombre = nullptr;
+}
--- Ejemplos clase/persona.h
+++ Ejemplos clase/persona.h
@@ -1,9 +1,34 @@
 class Persona
 {
+    /*
+    public: // Se puede acceder a los atributos y metodos desde cualquier parte del programa.
+    protected: // Se puede acceder a los atributos y metodos desde la misma clase y sus subclases.
+    private: // Solo se puede acceder a los atributos y metodos desde la misma clase.
+    */
+protected:
     int edad;
-    char nombre[20];
+    // char nombre[20];
+    char *nombre;
     void reir();
     void comer();
+
+public:
+    // los objetos pueden tener un constuctor, que es un metodo que se llama al crear el objeto.
+    // el constructor debe ser una funcion que tiene el mismo nombre de la clase. y no tiene tipo de retorno.
+    Persona(const char *el_nombre, int la_edad); // constructor por defecto
+    Persona();                                   // constructor por defecto
+
+    // En c++ podemos usar el mismo nombre para diferentes funciones, siempre y cuando los parametros sean diferentes
+    // Ciamdp implementamos funciones de este tipo, se le llama sobrecarga de funciones
+
+    // Los objetos pueden tener un destructor, El destructor es una funcion que se ejecuta automaticamente cuando el objeto es eliminado
+    // El destructor es una funcion que tiene el mismo nombre que la clase, pero precedido por el simbolo ~
+    ~Persona();
+
     void saludar();
     void hacerreir(int motivo);
+    void liberar();
 };
+
+// Protegido si se tiene subclases si se podran acceder a los atributos.
+// Con privado, las subclases no pueden acceder a los atributos.
